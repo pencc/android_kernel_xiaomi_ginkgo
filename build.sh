dfc6bf0b710e78ae88223f27fa94cc8566ae62c0
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2021-01-04 13:43:37 +0000

    build.sh : update
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- build.sh
+++ build.sh
@@ -1,44 +1,85 @@
-export PATH="$HOME/proton/bin:$PATH"
-SECONDS=0
-ZIPNAME="QuicksilveRV2-ginkgo-$(date '+%Y%m%d-%H%M').zip"
+#!/usr/bin/env bash
+#
+# Copyright (C) 2020 Edwiin Kusuma Jaya (MWG_Ryzen)
+#
+# Simple Local Kernel Build Script
+#
+# Configured for Redmi Note 8 / ginkgo custom kernel source
+#
+# Setup build env with akhilnarang/scripts repo
+#
+# Use this script on root of kernel directory
 
-if ! [ -d "$HOME/proton" ]; then
-echo "Proton clang not found! Cloning..."
-if ! git clone -q --depth=1 --single-branch https://github.com/kdrag0n/proton-clang ~/proton; then
-echo "Cloning failed! Aborting..."
-exit 1
+sudo ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+# Scrip option
+while (( ${#} )); do
+    case ${1} in
+        "-Z"|"--zip") ZIP=true ;;
+    esac
+    shift
+done
+
+[[ -z ${ZIP} ]] && { echo "${bold}Gunakan -Z atau --zip Untuk Membuat Zip Kernel Installer${normal}"; }
+
+# ENV
+CONFIG=vendor/ginkgo-perf_defconfig
+KERNEL_DIR=$(pwd)
+PARENT_DIR="$(dirname "$KERNEL_DIR")"
+KERN_IMG="/workspace/ryuzenn/out-A11/arch/arm64/boot/Image.gz-dtb"
+DTBO_IMG="/workspace/ryuzenn/out-A11/arch/arm64/boot/dtbo.img"
+export KBUILD_BUILD_USER="EdwiinKJ"
+export KBUILD_BUILD_HOST="RastaMod69"
+export PATH="/workspace/ryuzenn/toolchain/rastamod-clang/bin:$PATH"
+export LD_LIBRARY_PATH="/workspace/ryuzenn/toolchain/rastamod-clang/lib:$LD_LIBRARY_PATH"
+export KBUILD_COMPILER_STRING="$(/workspace/ryuzenn/toolchain/rastamod-clang/bin/clang --version | head -n 1 | perl -pe 's/\((?:http|git).*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')"
+export out=/workspace/ryuzenn/out-A11
+
+# Functions
+clang_build () {
+    make -j4 O=$out \
+                          ARCH=arm64 \
+                          CC="clang" \
+                          AR="llvm-ar" \
+                          NM="llvm-nm" \
+					      LD="ld.lld" \
+			              AS="llvm-as" \
+						  STRIP="llvm-strip" \
+			              OBJCOPY="llvm-objcopy" \
+			              OBJDUMP="llvm-objdump" \
+						  CLANG_TRIPLE=aarch64-linux-gnu- \
+                          CROSS_COMPILE=aarch64-linux-gnu-  \
+                          CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+
+# Build kernel
+make O=$out ARCH=arm64 $CONFIG > /dev/null
+echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
+clang_build
+
+if ! [ -a $KERN_IMG ]; then
+    echo "${bold}Build error, Tolong Perbaiki Masalah Ini${normal}"
+    exit 1
 fi
-fi
 
-mkdir -p out
-make O=out ARCH=arm64 vendor/ginkgo-perf_defconfig
+[[ -z ${ZIP} ]] && { exit; }
 
-if [[ $1 == "-r" || $1 == "--regen" ]]; then
-cp out/.config arch/arm64/configs/vendor/ginkgo-perf_defconfig
-echo -e "\nRegened defconfig succesfully!"
-exit 0
+# clone AnyKernel3
+if ! [ -d "AnyKernel3" ]; then
+    git clone https://github.com/kutemeikito/AnyKernel3
 else
-echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
+    echo "${bold}Direktori AnyKernel3 Sudah Ada, Tidak Perlu di Clone${normal}"
 fi
 
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
-git clone -q https://github.com/ghostrider-reborn/AnyKernel3
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-cp out/arch/arm64/boot/dtbo.img AnyKernel3
-rm -f *zip
-cd AnyKernel3
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-cd ..
-rm -rf AnyKernel3
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-if command -v gdrive &> /dev/null; then
-gdrive upload --share $ZIPNAME
-else
-echo "Zip: $ZIPNAME"
-fi
-rm -rf out/arch/arm64/boot
-else
-echo -e "\nCompilation failed!"
-fi
+# ENV
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+VENDOR_MODULEDIR="$ZIP_DIR/modules/vendor/lib/modules"
+STRIP="aarch64-linux-gnu-strip"
+
+# Make zip
+make -C "$ZIP_DIR" clean
+wifi_modules
+cp "$KERN_IMG" "$DTBO_IMG" "$ZIP_DIR"/
+make -C "$ZIP_DIR" normal
